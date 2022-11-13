8f7dd188b789bc412f404efc6e033d0d26887eb2
eliyahabba <eliya.habba@mail.huji.ac.il>
2022-11-13 19:49:40 +0200

    first version

--- main.py
+++ main.py
@@ -7,35 +7,30 @@
 # for index, row in df.iterrows():
 #     sentence = row['origin_sentence']
 
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
 
-import requests
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
 
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
 
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
+bucket_name = "streamlit-bucket"
+file_path = "all_results_with_origin_sen_clean.csv"
 
-    return None
+content = read_file(bucket_name, file_path)
 
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/spreadsheets/d/16GTfGGgAfsrgtxN9dRmDU-ZFwy0Wmu7G9OPR3Ue7-u0/edit?usp=share_link"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-    print(type(response))
-    # save_response_content(response, destination)
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
