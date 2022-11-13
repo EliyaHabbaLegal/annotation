ce6abfbceb60572b4ce3dfdfc7a4fe6d1b93afc9
eliyahabba <eliya.habba@mail.huji.ac.il>
2022-11-13 15:35:02 +0200

    first version

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 # Press Alt+Shift+X to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-C:\Users\t-eliyahabba\PycharmProjects\annotation_sentences\main.py
+import os
 
+import streamlit as st
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
-
+    os.makedirs("test", exist_ok=True)
+    st.write("test")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
