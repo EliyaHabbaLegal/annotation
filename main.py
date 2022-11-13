fd9fe924c56935e45f485095ba5742ff4244f8dc
eliyahabba <eliya.habba@mail.huji.ac.il>
2022-11-13 19:33:16 +0200

    first version

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-# This is a sample Python script.
+# # read df
+# import pandas as pd
+#
+# df = pd.read_csv(r'all_results_with_origin_sen_clean.csv')
+#
+# # loop over the df
+# for index, row in df.iterrows():
+#     sentence = row['origin_sentence']
 
-# Press Alt+Shift+X to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import os
 
-import streamlit as st
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
-    os.makedirs("test", exist_ok=True)
-    st.write("test")
+import requests
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/spreadsheets/d/16GTfGGgAfsrgtxN9dRmDU-ZFwy0Wmu7G9OPR3Ue7-u0/edit?usp=share_link"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+    print(type(response))
+    # save_response_content(response, destination)
